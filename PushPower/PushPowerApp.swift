578acec06ed77b39b943eec8d8de17d377f0582b
LukasHje <hjernquistlukas@gmail.com>
2024-10-07 14:12:05 +0200

    Initial Commit

---
+++ PushPower/PushPowerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PushPowerApp.swift
+//  PushPower
+//
+//  Created by Lukas Hjernquist on 2024-10-07.
+//
+
+import SwiftUI
+
+@main
+struct PushPowerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
