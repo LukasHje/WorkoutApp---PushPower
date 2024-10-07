d9c077e17eee6796c1ebaff3595dac9d1b8c463a
LukasHje <hjernquistlukas@gmail.com>
2024-10-07 21:32:27 +0200

    committing my Dial-view

---
+++ PushPower/Dial/Dial.swift
@@ -0,0 +1,54 @@
+//
+//  Dial.swift
+//  PushPower
+//
+//  Created by Lukas Hjernquist on 2024-10-07.
+//
+
+import Foundation
+import SwiftUI
+
+struct Dial: View {
+    
+    let goal: Int
+    let pushups: Int
+    
+    var body: some View {
+        ZStack {
+            SpokesView()
+                .padding(40)
+            CircleView()
+            
+            ZStack {
+                CircleView()
+                
+                Circle().stroke(style: StrokeStyle(lineWidth: 12))
+                    .padding(20)
+                    .foregroundColor(Color.foregroundGray)
+                
+                Circle()
+                    .trim(from: 0, to: CGFloat(pushups) / CGFloat(goal))
+                    .scale(x: -1)
+                    .rotation(.degrees(90))
+                    .stroke(style: StrokeStyle(lineWidth: 12, lineCap: .butt))
+                    .padding(20)
+                    .foregroundColor(Color.foregroundYellow)
+                
+                VStack {
+                    Text("Goal: \(goal)")
+                        .foregroundColor(Color.foregroundDeepBlue)
+                    Text("\(pushups)")
+                        .foregroundColor(Color.foregroundYellow)
+                        .font(.title)
+                        .bold()
+                        .padding()
+                    Text("Pushups")
+                        .foregroundColor(Color.foregroundDeepBlue)
+                }
+            }
+            .padding()
+            
+        }
+        .foregroundColor(Color.backgroundDeepGray)
+    }
+}
